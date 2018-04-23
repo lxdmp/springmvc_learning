89a0e1b9cc91d01abfb5128795e42c6ee1464384
lxdmp <yudmp@sina.com>
2018-04-23 18:29:05 +0800

    add process-time-log-interceptor

--- src/main/java/com/lxdmp/springtest/config/WebApplicationContextConfig.java
+++ src/main/java/com/lxdmp/springtest/config/WebApplicationContextConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import com.lxdmp.springtest.interceptor.ProcessTimeLogInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -18,6 +20,12 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
 	{
 		configurer.enable();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry)
+	{
+		registry.addInterceptor(new ProcessTimeLogInterceptor());
 	}
 
 	@Bean
---
+++ src/main/java/com/lxdmp/springtest/interceptor/ProcessTimeLogInterceptor.java
@@ -0,0 +1,39 @@
+package com.lxdmp.springtest.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.apache.log4j.Logger;
+
+public class ProcessTimeLogInterceptor implements HandlerInterceptor
+{
+	private static final Logger logger = Logger.getLogger(ProcessTimeLogInterceptor.class);
+	private static final String key_name = "startTime";
+	
+	public boolean preHandle(
+		HttpServletRequest request, HttpServletResponse response, Object handler)
+	{
+		long startTime = System.currentTimeMillis();
+		request.setAttribute(key_name, startTime);
+		return true; // 若返回false,请求不会到达控制器(controller)和其他拦截器(interceptor).
+	}
+	
+	public void postHandle(
+		HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+	{
+		String queryString = request.getQueryString() == null ?
+			"" : "?" + request.getQueryString();
+		String path = request.getRequestURL() + queryString;
+
+		long startTime = (Long)request.getAttribute(key_name);
+		long endTime = System.currentTimeMillis();
+		logger.info(String.format("%s millisecond taken to process the request %s.", endTime-startTime, path));
+	}
+
+	public void afterCompletion(
+		HttpServletRequest request, HttpServletResponse response, Object handler, Exception exceptionIfAny)
+	{
+	}
+}
+
