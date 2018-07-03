f7e02add11e732f2a230a43c94baf18204a037c0
lxdmp <yudmp@sina.com>
2018-07-03 15:46:32 +0800

    add something

--- src/main/java/com/lxdmp/springtest/formatter/CustomFormatTestFormatter.java
+++ src/main/java/com/lxdmp/springtest/formatter/CustomFormatTestFormatter.java
@@ -5,15 +5,26 @@ import com.lxdmp.springtest.domain.CustomFormatTestObj;
 
 public class CustomFormatTestFormatter implements Formatter<CustomFormatTestObj>
 {
+	private final String internal_sep = "_";
+	private final String external_sep = ";";
+
 	public String print(CustomFormatTestObj object, java.util.Locale locale)
 	{
-		return object.getId()+"_abc";
+		StringBuilder builder = new StringBuilder();
+		String[] l = object.getId().split(this.internal_sep);
+		for(String item : l)
+			builder.append(item).append(this.external_sep);
+		return builder.substring(0, builder.length()-1);
 	}
 
 	public CustomFormatTestObj parse(String text, java.util.Locale locale)
 	{
 		CustomFormatTestObj obj = new CustomFormatTestObj();
-		obj.setId(text+"_abc");
+		String[] l = text.split(this.external_sep);
+		StringBuilder builder = new StringBuilder();
+		for(String item : l)
+			builder.append(item).append(this.internal_sep);
+		obj.setId(builder.substring(0, builder.length()-1));
 		return obj;
 	}
 }
