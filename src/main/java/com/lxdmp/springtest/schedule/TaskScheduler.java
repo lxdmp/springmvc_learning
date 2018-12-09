4217c97c13e8a84ed58b4b0e89332955ee1bb0de
lxdmp <yudmp@sina.com>
2018-12-09 13:38:37 +0800

    add rabbit send in schedule

--- src/main/java/com/lxdmp/springtest/schedule/TaskScheduler.java
+++ src/main/java/com/lxdmp/springtest/schedule/TaskScheduler.java
@@ -4,12 +4,21 @@ import java.util.*;
 import java.text.SimpleDateFormat;
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.log4j.Logger;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.core.MessagePropertiesBuilder;
 
 @Component
 public class TaskScheduler
 {
 	private static final Logger logger = Logger.getLogger(TaskScheduler.class);
+
+	@Autowired 
+	private AmqpTemplate rabbitTemplate;
 	
 	// 格式与cron一致(与cron相比多了秒的精度,每天hh:mm:ss执行)
 	@Scheduled(cron = "0 30 12 * * *")
@@ -25,6 +34,27 @@ public class TaskScheduler
 	{
 		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		//logger.info(String.format("periodic work : %s", f.format(new Date())));
+	}
+
+	@Scheduled(fixedRate = 1000*10)
+	public void testForRabbitSend()
+	{
+		MessageProperties props = MessagePropertiesBuilder.newInstance()
+			.setContentType(MessageProperties.CONTENT_TYPE_TEXT_PLAIN)
+			//.setMessageId("123")
+			//.setHeader("bar", "baz")
+			.build();
+		Message message = MessageBuilder
+			.withBody(
+				String.format("%s", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())).getBytes()
+			)
+			.andProperties(props)
+			.build();
+		rabbitTemplate.send(
+			"test.topic", // exchange
+			"test.abc", // routing-key
+			message
+		);
 	}
 }
 
